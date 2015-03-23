60a81490724e2cd447f16566534365fb778efe62
Chris Aumann <me@chr4.org>
2015-03-23 12:11:18 +0100

    Bump cookbook and graylog version to 1.0.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,11 +18,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-default['graylog']['web_interface']['version'] = '1.0.0'
+default['graylog']['web_interface']['version'] = '1.0.1'
 default['graylog']['web_interface']['user'] = 'graylog2'
 default['graylog']['web_interface']['url'] = "https://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{node['graylog']['web_interface']['version']}.tgz"
 
-default['graylog']['server']['version'] = '1.0.0'
+default['graylog']['server']['version'] = '1.0.1'
 default['graylog']['server']['user'] = 'graylog2'
 default['graylog']['server']['group'] = 'graylog2'
 default['graylog']['server']['url'] = "https://packages.graylog2.org/releases/graylog2-server/graylog-#{node['graylog']['server']['version']}.tgz"
